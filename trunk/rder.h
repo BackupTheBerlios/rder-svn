66b1262242f7cc4d70dd1dabe129f27c51f3e2be
mimamu <mimamu@7a6d173e-4bf7-0310-92a1-c6a3efc6f8d7>
2006-02-24 15:42:20 +0000

    File(s) to commit:  rder.h #### update.

--- trunk/rder.h
+++ trunk/rder.h
@@ -1,2 +1,12 @@
+#include <Rinternals.h>
+
 void initR(void);
+
+// for cR
+SEXP get_fun_from_name(char *ident);
+char *get_last_error_msg();
+
+// for R2R
+VALUE to_RubyObj(SEXP robj);
+SEXP to_RObj(VALUE ruby_obj);
 
