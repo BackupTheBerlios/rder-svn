a9b0a658c5e84538113919c36f5f64ab7683f920
mimamu <mimamu@7a6d173e-4bf7-0310-92a1-c6a3efc6f8d7>
2006-02-25 21:53:41 +0000

    File(s) to commit:  lib/rder.rb #### begin re-construction.

--- trunk/lib/rder.rb
+++ trunk/lib/rder.rb
@@ -1,40 +1,16 @@
-require "rder/rder.so"
+require "rder.so"
 
 module RdeR
 
   class R
     
-    def print
-      self.to_s
-    end
-
     def method_missing(symbol, *args)
-      # We need manipurations of "[", "<-" etc.
+      # We need manipurations of "[", "[[", "<-" etc.
       fname = symbol.to_s
 
-      args = args.map { |arg| Robj.new(arg) }
-p args
-
-      fetch_R(fname)
+      robj = Robj.new(fname, args)
     end
-    
-    def rexpr(symbol, *args)
-      "#{symbol}(#{args.join(", ")})"
-    end
-
-private
-
-    def fetch_R(expr)
-      begin
-        res = evalR(expr)
-#        res = evalR('ls')
-      rescue
-        raise RException, 'R Evaluation error.'
-      end
-      
-      res
-    end
-    
+        
   end
 
 end
